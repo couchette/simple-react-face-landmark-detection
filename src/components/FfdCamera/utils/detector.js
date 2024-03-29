37fc6bf38f04ea4c33691f41641e3184dc63f2f3
Luke Wei <couchette@aliyun.com>
2024-03-29 22:00:45 +0800

    load model from public/models

--- src/components/FfdCamera/utils/detector.js
+++ src/components/FfdCamera/utils/detector.js
@@ -1,15 +1,19 @@
 import * as faceLandmarksDetection from "@tensorflow-models/face-landmarks-detection";
 import { drawMesh } from "./drawMesh";
+
 export const runDetector = async (video, canvas, setPredictResult) => {
   console.log("runDetector");
   const model = faceLandmarksDetection.SupportedModels.MediaPipeFaceMesh;
   const detectorConfig = {
     runtime: "tfjs",
+    detectorModelUrl: "models/face-detect-short/model.json",
+    landmarkModelUrl: "models/face-mesh/model.json",
   };
   const detector = await faceLandmarksDetection.createDetector(
     model,
     detectorConfig
   );
+
   const detect = async (net) => {
     const estimationConfig = { flipHorizontal: false };
     const faces = await net.estimateFaces(video, estimationConfig);
